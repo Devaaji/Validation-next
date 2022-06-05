bcc846458d95ecae839c90833365b5755ec9d364
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-06-05 21:32:35 +0700

    install  package Nprogressbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import NextNProgress from 'nextjs-progressbar'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <NextNProgress height={5} options={{ easing: "ease", speed: 500, showSpinner: false }} />
+  <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
